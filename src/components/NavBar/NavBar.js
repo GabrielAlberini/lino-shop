aff1255f3365e738015efa60afcc14d15e7125ff
CuerpoCasa <gabialberini733@gmail.com>
2021-11-18 22:51:02 -0300

    V1.2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NavBar.css'
+import logo from '../../lino-logo.jpg'
+
+
+export default function NavBar() {
+    
+    return (
+        <nav className="NavBar">
+            <div className="container-logo">
+                <div className="container-logo-content">
+                    <a href="#"><img className="logo-img" src={ logo } alt="logo ecommerce lino"/>
+                    </a>
+                </div>
+            </div>
+            <div className="container-list">
+                <ul className="container-list-content">
+                    <li className="list-option"><a href="#" className="list-option-link">Home</a></li>
+                    <li className="list-option"><a href="#" className="list-option-link">Products</a></li>
+                    <li className="list-option"><a href="#" className="list-option-link">Contact</a></li>
+                    <li className="list-option"><a href="#" className="list-option-link">About us</a></li>
+                    <li className="list-option"><a href="#" className="list-option-link">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
